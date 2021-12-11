94b3c8de05a6c8a85b90ad250b29fb56ce6be3b2
Liana <lianavagyan.90@gmail.com>
2021-12-11 04:20:11 +0400

    feat: implementation was done and adde

---
+++ myForEach.js
@@ -0,0 +1,17 @@
+// Implementation of Array.forEach() method
+
+const array = [1, 2, 3, 4, 5];
+
+function myForEach(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i, arr);
+  }
+}
+
+console.log(
+  myForEach(array, (el, i) => {
+    console.log(`${i}: ${el}`);
+  })
+);
+
+console.log(array.forEach((el, i) => console.log(`${i}: ${el}`)));
